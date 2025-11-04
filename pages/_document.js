c884f0f938ecf913bcd8b451dcaf21129453dcff
bizleap1 <bizleap1@gmail.com>
2025-11-04 15:10:16 +0530

    ujpdated

--- pages/_document.js
+++ pages/_document.js
@@ -4,7 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/Fav.png" />
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500&family=Poppins:wght@600;700&display=swap" rel="stylesheet" />
         <meta name="description" content="A & T Buildcon - Engineers and Builders" />
       </Head>
